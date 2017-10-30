f0b643c159bc8bd6ce9e9030df53de04c2bd103c
test <diachenkoiaroslav@gmail.com>
2017-10-30 23:26:05 +0200

    update

--- Module1/resources/HomeWork/2.sql
+++ Module1/resources/HomeWork/2.sql
@@ -1,6 +1,6 @@
 #2. Найти самый дорогой проект (исходя из ЗП разработчиков).
 
-# SOLUTION_1:
+# SOLUTION_1 (via sorting):
 
 
 USE Module1_HomeWork;
@@ -11,7 +11,7 @@ GROUP BY projects.name
 ORDER BY sum(developers.salary) DESC
 LIMIT 1;
 
-# SOLUTION_2:
+# SOLUTION_2 (via nested subquery):
 
 USE Module1_HomeWork;
 
@@ -33,3 +33,19 @@ JOIN (
               AS table2
       ) AS table3
 ON table1.project_cost = table3.max_cost;
+
+# SOLUTION_3 (using temporary tables):
+
+DROP TABLE IF EXISTS TABLE1;
+DROP TABLE IF EXISTS TABLE2;
+
+CREATE TEMPORARY TABLE TABLE1 AS (
+  SELECT (projects.name) AS name, sum(developers.salary) AS project_spending
+  FROM projects, developers, projects_developers
+  WHERE projects.id = projects_developers.project_id and developers.id = projects_developers.developer_id
+  GROUP BY projects.id);
+
+CREATE TEMPORARY TABLE TABLE2 AS (
+  SELECT max(TABLE1.project_spending) AS max_project_spending FROM TABLE1);
+
+SELECT TABLE1.name, TABLE1.project_spending FROM TABLE1,TABLE2 WHERE TABLE1.project_spending = TABLE2.max_project_spending;
